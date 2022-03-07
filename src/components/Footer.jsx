3d2f807f6f12de0aa7da13ea4ff77064f65736fd
PedroFolego <hmfpedro@hotmail.com>
2022-03-07 18:26:14 -0300

    finaliza reqs

---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer({ history }) {
+  return (
+    <footer data-testid="footer" className="footer">
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+        data-testid="drinks-bottom-btn"
+      >
+        <img src={ drinkIcon } alt="drink" />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+        data-testid="explore-bottom-btn"
+      >
+        <img src={ exploreIcon } alt="explore" />
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/drinks') }
+        data-testid="food-bottom-btn"
+      >
+        <img src={ mealIcon } alt="meal" />
+      </button>
+    </footer>
+  );
+}
+
+export default Footer;
+
+Footer.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
