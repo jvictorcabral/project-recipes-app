c8d00109e1138744474f72fc14b11d5d5b0e3484
camila <camilaczeg@gmail.com>
2022-03-07 18:23:19 -0300

    atualiza rotas

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="foods" component={ Login } />
+      <Route exact path="/foods" component={ Login } />
       <Route exact path="/drinks" component={ Login } />
       <Route exact path="/foods/:id" component={ Login } />
       <Route exact path="/drinks/:id" component={ Login } />
-      <Route exact path="foods/:id/in-progress" component={ Login } />
-      <Route exact path="drinks/:id/in-progress" component={ Login } />
+      <Route exact path="/foods/:id/in-progress" component={ Login } />
+      <Route exact path="/drinks/:id/in-progress" component={ Login } />
       <Route exact path="/explore" component={ Login } />
-      <Route exact path="explore/foods" component={ Login } />
-      <Route exact path="explore/drinks" component={ Login } />
-      <Route exact path="explore/foods/ingredients" component={ Login } />
-      <Route exact path="explore/drinks/ingredients" component={ Login } />
-      <Route exact path="explore/foods/nationalities" component={ Login } />
+      <Route exact path="/explore/foods" component={ Login } />
+      <Route exact path="/explore/drinks" component={ Login } />
+      <Route exact path="/explore/foods/ingredients" component={ Login } />
+      <Route exact path="/explore/drinks/ingredients" component={ Login } />
+      <Route exact path="/explore/foods/nationalities" component={ Login } />
       <Route exact path="/profile" component={ Login } />
       <Route exact path="/done-recipes" component={ Login } />
       <Route exact path="/favorite-recipes" component={ Login } />
