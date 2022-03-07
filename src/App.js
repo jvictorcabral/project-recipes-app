ded8cad11343397653c41615216aa932346194f2
camila <camilaczeg@gmail.com>
2022-03-07 18:19:33 -0300

    adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './styles/App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="foods" component={ Login } />
+      <Route exact path="/drinks" component={ Login } />
+      <Route exact path="/foods/:id" component={ Login } />
+      <Route exact path="/drinks/:id" component={ Login } />
+      <Route exact path="foods/:id/in-progress" component={ Login } />
+      <Route exact path="drinks/:id/in-progress" component={ Login } />
+      <Route exact path="/explore" component={ Login } />
+      <Route exact path="explore/foods" component={ Login } />
+      <Route exact path="explore/drinks" component={ Login } />
+      <Route exact path="explore/foods/ingredients" component={ Login } />
+      <Route exact path="explore/drinks/ingredients" component={ Login } />
+      <Route exact path="explore/foods/nationalities" component={ Login } />
+      <Route exact path="/profile" component={ Login } />
+      <Route exact path="/done-recipes" component={ Login } />
+      <Route exact path="/favorite-recipes" component={ Login } />
+    </Switch>
   );
 }
 
