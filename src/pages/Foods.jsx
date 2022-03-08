33b6cfd8a88ffc43e63fc32233118feefb135e73
camila <camilaczeg@gmail.com>
2022-03-08 15:29:58 -0300

    requisitos 25 e 26: renderiza 12 cards de receitas na pagina

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropType from 'prop-types';
 import CategoryFilters from '../components/CategoryFilters';
+import fetchMeals from '../services/mealsApi';
+import RecipeCard from '../components/RecipeCard';
 
 function Foods({ location: { pathname } }) {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const MEALS_QUANTITY = 12;
+    const getMeals = async () => {
+      const results = await fetchMeals('name', '');
+      setMeals(results.meals.slice(0, MEALS_QUANTITY));
+    };
+    getMeals();
+  }, []);
+
   return (
     <div>
       <CategoryFilters pathname={ pathname } />
+      {meals.map(({ idMeal, strMealThumb, strMeal }, index) => (
+        <RecipeCard
+          key={ idMeal }
+          img={ strMealThumb }
+          name={ strMeal }
+          index={ index }
+        />
+      ))}
     </div>
   );
 }
