b8130ca1fcd0a86615e2efb356174b1d727ae8e1
Luana Eugênia <luana.san2015.ls@gmail.com>
2022-03-08 00:06:05 -0300

    Requisito 14 - API configurado para a barra de busca.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,53 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../service/api';
+
+const state = {};
+
+function onChangeSearchText(event) {
+  state.setSearchText(event.target.value);
+}
+
+function onChangeOption(event) {
+  state.setOption(event.target.value);
+}
+
+function getRequest(event) {
+  event.preventDefault();
+
+  if (state.option === 'firstLetter' && state.searchText.length > 1) {
+    global.alert('Your search must have only 1 (one) character');
+  } else {
+    api(state.option, state.searchText)
+      .then((response) => console.log(response));
+  }
+}
 
 function SearchBar() {
+  [state.searchText, state.setSearchText] = useState('');
+  [state.option, state.setOption] = useState('');
+
   return (
-    <div>
-      <form>
-        <div>
-          <input type="text" placeholder="Search Reciper" data-testid="search-input" />
-        </div>
+    <form>
+      <div>
+        <input
+          type="text"
+          value={ state.searchText }
+          onChange={ onChangeSearchText }
+          placeholder="Search Reciper"
+          data-testid="search-input"
+        />
+      </div>
 
-        <div>
-          <label htmlFor="ingredient">
-            <input
-              id="ingredient"
-              type="radio"
-              name="filter"
-              value="Ingredient"
-              data-testid="ingredient-search-radio"
-            />
-            Ingredient
-          </label>
+      <div>
+        <label htmlFor="ingredient">
+          <input
+            id="ingredient"
+            type="radio"
+            name="filter"
+            value="ingredient"
+            onChange={ onChangeOption }
+            data-testid="ingredient-search-radio"
+          />
+          Ingredient
+        </label>
 
-          <label htmlFor="name">
-            <input
-              id="name"
-              type="radio"
-              name="filter"
-              value="Name"
-              data-testid="name-search-radio"
-            />
-            Name
-          </label>
+        <label htmlFor="name">
+          <input
+            id="name"
+            type="radio"
+            name="filter"
+            value="name"
+            onChange={ onChangeOption }
+            data-testid="name-search-radio"
+          />
+          Name
+        </label>
 
-          <label htmlFor="firstLetter">
-            <input
-              id="firstLetter"
-              type="radio"
-              name="filter"
-              value="FirstLetter"
-              data-testid="first-letter-search-radio"
-            />
-            First Letter
-          </label>
-        </div>
+        <label htmlFor="firstLetter">
+          <input
+            id="firstLetter"
+            type="radio"
+            name="filter"
+            value="firstLetter"
+            onChange={ onChangeOption }
+            data-testid="first-letter-search-radio"
+          />
+          First Letter
+        </label>
+      </div>
 
-        <div>
-          <button type="submit" data-testid="exec-search-btn">Search</button>
-        </div>
-      </form>
-    </div>
+      <div>
+        <button
+          type="submit"
+          onClick={ getRequest }
+          data-testid="exec-search-btn"
+        >
+          Search
+        </button>
+      </div>
+    </form>
   );
 }
 
---
+++ src/service/api.js
@@ -0,0 +1,10 @@
+export default (option, text) => {
+  let optionQuery = '';
+
+  if (option === 'ingredient') { optionQuery = 'filter.php?i'; }
+  if (option === 'name') { optionQuery = 'search.php?s'; }
+  if (option === 'firstLetter') { optionQuery = 'search.php?f'; }
+
+  return fetch((`https://www.themealdb.com/api/json/v1/1/${optionQuery}=${text}`))
+    .then((response) => response.json());
+};
