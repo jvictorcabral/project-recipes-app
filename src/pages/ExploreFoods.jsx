4311520ee6d8fff088d15f3ca3f55cd5aeab3ac2
camila <camilaczeg@gmail.com>
2022-03-11 19:15:19 -0300

    requisito 74: surprise me in foods

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import fetchRandomMeal from '../services/randomMeal';
 
 function ExploreFoods({ history }) {
+  const getRandomMeal = async () => {
+    const id = await fetchRandomMeal();
+    console.log(id);
+    history.push(`/foods/${id}`);
+  };
+
   return (
     <main>
       <Header title="Explore Foods" />
-      <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
-      <button type="button" data-testid="explore-by-nationality">By Nationality</button>
-      <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      <button type="button" data-testid="explore-by-ingredient">
+        By Ingredient
+      </button>
+      <button type="button" data-testid="explore-by-nationality">
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ getRandomMeal }
+      >
+        Surprise me!
+      </button>
       <Footer history={ history } />
     </main>
   );
