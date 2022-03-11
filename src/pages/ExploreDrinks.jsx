64e50d5bab80d7068dab9756999f9023eadbd1c4
camila <camilaczeg@gmail.com>
2022-03-11 19:25:42 -0300

    requisito 74: completo!!

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import fetchRandomDrink from '../services/randomDrink';
 
 function ExploreDrinks({ history }) {
+  const getRandomDrink = async () => {
+    const id = await fetchRandomDrink();
+    console.log(id);
+    history.push(`/drinks/${id}`);
+  };
+
   return (
     <main>
       <Header title="Explore Drinks" />
       <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
-      <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ getRandomDrink }
+      >
+        Surprise me!
+      </button>
       <Footer history={ history } />
     </main>
   );
