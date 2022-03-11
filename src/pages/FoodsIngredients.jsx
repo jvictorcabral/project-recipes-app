e924c69255ba5af5f45343c396418c2a9c43564f
PedroFolego <hmfpedro@hotmail.com>
2022-03-11 19:34:49 -0300

    começando requisitos

--- src/pages/FoodsIngredients.jsx
+++ src/pages/FoodsIngredients.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import fetchIngredients from '../services/ingredients';
 
-function FoodsIngredients({ history }) {
+function FoodsIngredients({ history, location: { pathname } }) {
+  const [ingredients, setIngredients] = useState([]);
+
+  const getIngredients = async () => {
+    const MAX_RECOMENDATION = 12;
+    const ingredientsAPI = await fetchIngredients(pathname);
+    setIngredients(
+      ingredientsAPI.filter((a, index) => index < MAX_RECOMENDATION),
+    );
+  };
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
   return (
     <main>
-      <Header title="Ingredientizinhos das Comidinhas" />
+      <Header title="Foods Ingredients" />
+      {ingredients.map(({ strIngredient }, index) => (
+        <div data-testid={ `${index}-card-name` } key={ strIngredient }>
+          <p data-testid={ `${index}-ingredient-card` }>
+            {strIngredient}
+          </p>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ `https://www.themealdb.com/images/ingredients/${strIngredient}.png` }
+            alt={ strIngredient }
+          />
+        </div>
+      ))}
       <Footer history={ history } />
     </main>
   );
