6f533d198542340922779014a4ba0c406ac6c984
camila <camilaczeg@gmail.com>
2022-03-11 18:50:56 -0300

    requsito 72~

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -4,10 +4,21 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreDrinks({ history }) {
+  const handleClick = ({ target }) => {
+    history.push(`/explore/drinks/${target.name}`);
+  };
+
   return (
     <main>
       <Header title="Explore Drinks" />
-      <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      <button
+        type="button"
+        name="ingredients"
+        data-testid="explore-by-ingredient"
+        onClick={ handleClick }
+      >
+        By Ingredient
+      </button>
       <button type="button" data-testid="explore-surprise">Surprise me!</button>
       <Footer history={ history } />
     </main>
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -4,12 +4,27 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreFoods({ history }) {
+  const handleClick = ({ target }) => {
+    history.push(`/explore/foods/${target.name}`);
+  };
+
   return (
     <main>
       <Header title="Explore Foods" />
-      <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
-      <button type="button" data-testid="explore-by-nationality">By Nationality</button>
-      <button type="button" data-testid="explore-surprise">Surprise me!</button>
+      <button
+        type="button"
+        name="ingredients"
+        data-testid="explore-by-ingredient"
+        onClick={ handleClick }
+      >
+        By Ingredient
+      </button>
+      <button type="button" data-testid="explore-by-nationality">
+        By Nationality
+      </button>
+      <button type="button" data-testid="explore-surprise">
+        Surprise me!
+      </button>
       <Footer history={ history } />
     </main>
   );
